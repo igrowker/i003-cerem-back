0a2d042506bdc975041c08a3ffc3aab043f756de
0rellano <orellanojoaquinmarcelo@gmail.com>
2024-09-11 17:12:26 -0400

    Connection of application "api" with project

--- site_app/config/settings.py
+++ site_app/config/settings.py
@@ -30,15 +30,24 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
 ]
+
+THIRD_APPS = [
+    'rest_framework',
+]
+
+LOCAL_APPS = [
+    'api'
+]
+
+INSTALLED_APPS = BASE_APPS + THIRD_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
