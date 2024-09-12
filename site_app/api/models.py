8fa55a1907c5a0243dab1e41e74f9c4a08138866
0rellano <orellanojoaquinmarcelo@gmail.com>
2024-09-12 17:49:59 -0300

    creating django user model and migrate

--- site_app/api/models.py
+++ site_app/api/models.py
@@ -1,11 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 
-class Usuario(models.Model):
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, name, password, is_staff, is_superuser, **extra_fields):
+        user = self.model(
+            email=email,
+            name=name,
+            is_staff=is_staff,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_user(self, email, name, password=None, **extra_fields):
+        return self._create_user(email, name, password, False, **extra_fields)
+
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        return self._create_user(email, name, password, True, **extra_fields)
+
+
+class Usuario(AbstractBaseUser):
+    class Meta:
+        db_table = 'usuarios'
+
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nombre']
+
+    def __str__(self) -> str:
+        return f"Usuario {self.id}. {self.email}"
+    
 
 
 class Cliente(models.Model):
