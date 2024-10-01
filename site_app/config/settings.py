8996f551549fc952928c33569887018ffda8479b
Judit Alvarez Rubio <160534450+juditsoria@users.noreply.github.com>
2024-10-01 14:22:56 +0000

    settings

--- site_app/config/settings.py
+++ site_app/config/settings.py
@@ -2,28 +2,24 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-
 # Cargar variables de entorno .env
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tdbajof^6om84qix%vxin+9hes2@^i$1@s%xu^bkh4umy$r#(g'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-tdbajof^6om84qix%vxin+9hes2@^i$1@s%xu^bkh4umy$r#(g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -32,30 +28,28 @@ BASE_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+ROOT_URLCONF = 'config.urls'
 THIRD_APPS = [
     'rest_framework',
-
-INSTALLED_APPS = [
-
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'oauth2_provider',
-
 ]
+
+LOCAL_APPS = [
+    'api',
+]
+
+# Combina todas las aplicaciones en una sola lista
+INSTALLED_APPS = BASE_APPS + THIRD_APPS + LOCAL_APPS
 
 AUTHENTICATION_BACKENDS = [
-    # Django's ModelBackend for normal users
     'django.contrib.auth.backends.ModelBackend',
-    # `allauth` specific authentication backend
-    'allauth.account.auth_backends.AuthenticationBackend'
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
-
-
-INSTALLED_APPS = BASE_APPS + THIRD_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -68,16 +62,14 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
-#                                                                 CONFIGURACION OAUTH PARA SITIO -- METODO 1
-
 # Configura el dominio del sitio
 SITE_ID = 1
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'APP': {
-            'client_id': 'TU_CLIENT_ID',
-            'secret': 'TU_CLIENT_SECRET',
+            'client_id': os.getenv('GOOGLE_CLIENT_ID', 'TU_CLIENT_ID'),
+            'secret': os.getenv('GOOGLE_CLIENT_SECRET', 'TU_CLIENT_SECRET'),
             'scope': ['profile', 'email']
         }
     }
@@ -88,6 +80,7 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -106,38 +99,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# BASE DE DATOS LOCAL. DESCOMENTAR SI SE QUIERE UTILIZAR
-# POSIBLEMENTE NECESITARA APLICAR LAS MIGRACIOENS
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# BASE DE DATOS: POSTGRESQL
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",  # Asegúrate de que este sea el nombre correcto
+        "NAME": "postgres",
         "USER": "postgres.zpipmvsqazynfzoggdzd",
         "PASSWORD": "YJjZ8EGQPqXUtfI8",
         "HOST": "aws-0-sa-east-1.pooler.supabase.com",
         "PORT": "6543",
         "TEST": {
-            "NAME": "test_postgres_v2",  #
+            "NAME": "test_postgres_v2",
         },
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -153,27 +130,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Selected USER
