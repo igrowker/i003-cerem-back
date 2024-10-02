5d886ec949ed64dee24ca165c697eb2fd2175ee8
Judit Alvarez Rubio <160534450+juditsoria@users.noreply.github.com>
2024-10-02 20:09:57 +0000

    correcciones

--- site_app/api/services/__init__.py
+++ site_app/api/services/__init__.py
@@ -1,1 +1,6 @@
+from api.services.CampanaService import CampanaService
+from api.services.Estadisticas import EstadisticaCampana
+from api.services.google_calendarService import GoogleCalendarService
+from api.services.ClienteService import ClienteService
+
 
