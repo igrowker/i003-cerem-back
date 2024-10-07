289f1da93e43de25317d029b54ae9a969171eb6e
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-10-07 16:01:34 -0300

    no message

--- site_app/config/settings.py
+++ site_app/config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-tdbajof^6om84qix%vxin+9hes2@^i$1@s%xu^bkh4umy$r#(g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['i003-cerem-back.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
