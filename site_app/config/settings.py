726fc9faa6a11368a5b40a8587f2e5de55dbcaf9
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-10-12 18:41:09 -0300

    no message

--- site_app/config/settings.py
+++ site_app/config/settings.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 
 # Cargar variables de entorno .env
 load_dotenv()
-
+PORT = os.environ.get('PORT', '8000')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
