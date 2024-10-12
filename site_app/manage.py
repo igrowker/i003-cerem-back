e792ef7e3f699f050d238401dec65605b33588a5
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-10-12 18:14:59 -0300

    no message

--- site_app/manage.py
+++ site_app/manage.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os, sys
-
-
+import os
+import sys
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    # Leer el puerto de la variable de entorno PORT en Render
+    port = os.environ.get('PORT', '8000')  # Usa el puerto de Render o 8000 por defecto
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +17,12 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    # Modifica para correr el servidor en el puerto especificado
+    if len(sys.argv) == 1:
+        sys.argv += ['runserver', f'0.0.0.0:{port}']
+
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
